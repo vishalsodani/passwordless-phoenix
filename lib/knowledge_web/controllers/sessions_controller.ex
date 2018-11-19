73ff59e062155a72885b0fd4221506ffda437f69
Vishal Sodani <vishalsodani@rediffmail.com>
2018-11-19 14:49:38 +0530

    just put flash message and redirect

--- lib/knowledge_web/controllers/sessions_controller.ex
+++ lib/knowledge_web/controllers/sessions_controller.ex
@@ -6,7 +6,9 @@ defmodule KnowledgeWeb.SessionsController do
     end
 
     def create(conn, %{"session" => %{"email" => email}}) do
-        
+        conn
+        |> put_flash(:info, "We have sent you a link for signing in via email to #{email}.")
+        |> redirect(to: Routes.page_path(conn, :index))
     end
 
     def delete(conn, _params) do
