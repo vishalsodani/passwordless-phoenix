386e7ff7fa4e8ad05eed69050f429bbff54196cb
Vishal Sodani <vishalsodani@rediffmail.com>
2018-11-20 09:53:55 +0530

    extract value from session; how to use logger to log to console

--- lib/knowledge_web/controllers/page_controller.ex
+++ lib/knowledge_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule KnowledgeWeb.PageController do
+  require Logger
   use KnowledgeWeb, :controller
 
   def index(conn, _params) do
+    user_id = get_session(conn, :user_id)
+    Logger.info user_id
     render(conn, "index.html")
   end
 end
